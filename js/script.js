7eb6f5aa67ab819214cc90c50d2e811a19090e57
shishioZXC <niksizdog@gmail.com>
2026-04-27 17:14:00 +0300

    feat: mobile version done

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
 var toggle = document.querySelector(".nav-toggle");
 var menu = document.querySelector(".nav-list");
-var countDownDate = new Date("Apr 30, 2026 15:05:30").getTime();
 var timer = document.querySelector(".timer")
 toggle.addEventListener("click", function() {
     console.log("click");
     menu.classList.toggle("opened")
 })
 
-// setInterval(function() {
-//     var nowDate = new Date().getTime();
-//     var distance = countDownDate - nowDate;
-//     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-//     var seconds = Math.floor((distance % (1000 * 60)) / (1000));
-
-//     timer.textContent = hours + "hrs " + minutes + "mins " + seconds + "sec";
-// })
+var x = setInterval(function() {
+    const countDownDate = new Date("Apr 27, 2026 16:03:00").getTime();
+    const nowDate = new Date().getTime();
+    var distance = countDownDate - nowDate;
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / (1000));
+    timer.textContent = hours + "hrs " + minutes + "mins " + seconds + "sec";
+    if (distance <= 0) {
+        clearInterval(x);
+        timer.textContent = "Акция закончилась";
+    }
+}, 1000)
