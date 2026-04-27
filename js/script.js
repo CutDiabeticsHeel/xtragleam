bfc07cd3185fa944ca0dde18f7f27517c5accb25
shishioZXC <niksizdog@gmail.com>
2026-04-27 15:28:22 +0300

    feat: timer

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 var toggle = document.querySelector(".nav-toggle");
-var menu = document.querySelector(".nav-list")
-
+var menu = document.querySelector(".nav-list");
+var countDownDate = new Date("Apr 30, 2026 15:05:30").getTime();
+var timer = document.querySelector(".timer")
 toggle.addEventListener("click", function() {
     console.log("click");
     menu.classList.toggle("opened")
 })
+
+// setInterval(function() {
+//     var nowDate = new Date().getTime();
+//     var distance = countDownDate - nowDate;
+//     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//     var seconds = Math.floor((distance % (1000 * 60)) / (1000));
+
+//     timer.textContent = hours + "hrs " + minutes + "mins " + seconds + "sec";
+// })
