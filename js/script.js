5b00feb87b0d3e290451124ef9609379e603782a
shishioZXC <niksizdog@gmail.com>
2026-05-02 19:03:47 +0300

    feat: categories filter

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 var toggle = document.querySelector(".nav-toggle");
 var cross = document.querySelector(".nav-list__svg")
 var menu = document.querySelector(".nav-list");
-var timer = document.querySelector(".timer")
-const countDownDate = new Date().getTime() + 24 * 60 * 60 * 1000;
+var timer = document.querySelector(".timer");
 toggle.addEventListener("click", () => menu.classList.add("opened"))
 cross.addEventListener("click", () => menu.classList.remove("opened"))
 
+var timer = document.querySelector(".timer");
+const countDownDate = new Date().getTime() + 24 * 60 * 60 * 1000;
 var x = setInterval(function() {
     const nowDate = new Date().getTime();
     var distance = countDownDate - nowDate;
@@ -18,3 +19,29 @@ var x = setInterval(function() {
         timer.textContent = "Акция закончилась";
     }
 }, 1000)
+
+const categories = document.querySelectorAll(".categories__button");
+const products = document.querySelectorAll(".product-list__item");
+const viewAll = document.querySelector(".catalog__link")
+viewAll.addEventListener("click", function(){
+    for (const product of products) {
+        product.style.display = "flex";
+    }
+})
+for (const category of categories) {
+    category.addEventListener("click", function() {
+        const selectedCategory = category.getAttribute("data-categories");
+        for (const product of products) {
+            if (selectedCategory == product.getAttribute("data-categories")){
+                product.style.display = "flex";
+            } else {
+                product.style.display = "none";
+            }
+            window.scrollTo({
+                    top: 1700,
+                    left: 0,
+                    behavior: "smooth",
+            });
+        }
+    });
+}
